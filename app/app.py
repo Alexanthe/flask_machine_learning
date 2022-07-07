e12f7df2f4d7ac3ef77f8fd440704b656822ce8a
Alexanthe <81797141+Alexanthe@users.noreply.github.com>
2022-07-07 23:01:33 +0800

    Remove { and updated the ML model and added comments for app.py and change the validation to > 0.6

--- app/app.py
+++ app/app.py
@@ -7,9 +7,11 @@
 from flask import Flask, render_template, request, redirect
 
 import sys
+import random
+import string
 
-# sys.path.append(r"C:\Users\alexa\Documents\GitHub\flask_machine_learning\app\model")
-# sys.path.append(".\model")
+# import mysql.connector
+
 import model.models as models
 
 # import preprocess
@@ -30,6 +32,32 @@ def index():
         data_list.append(sentence_1)
         sim_msg = models.over_model(data_list)
         result["sim_msg"] = sim_msg
+
+        #     mydb = mysql.connector.connect(
+        #     host="edc3a984-eedd-4396-851b-823320a16114.mysql.service.internal",
+        #     user="87188e870a444fa7b278aa0622f327ce",
+        #     password="qvf8x0ttgzvvhqzj",
+        #     db="service_instance_db",
+        # )
+        #     # get len of database and declare in insert
+        #     mycursor = mydb.cursor(dictionary=True)
+        #     mycursor.execute('SELECT count(id) FROM current_cases')
+        #     lenDb = mycursor.fetchone()
+        #     lenDb = str(lenDb)
+
+        #     # do list of name, age, ic
+        #     name = ["Alex","James","Ashley","Debbie"]
+        #     name = random.choice(name)
+
+        #     age = str(random.randint(21,65))
+
+        #     ic ='S'+age+''.join([random.choice(string.digits
+        #         + string.digits) for n in range(5)]) + ''.join([random.choice(string.ascii_letters
+        #         + string.digits) for n in range(1)]).upper()
+
+        # fix location, and further prediction and other things
+        # put sentence_1 and sim_msg in symptoms and tag123 respectively
+        # connection
         return render_template("index.html", result=result)
 
 
